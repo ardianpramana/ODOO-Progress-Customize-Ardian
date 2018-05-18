033291f0c53f6d373f7b46ef3442a19415e988cb
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-19 01:45:00 +0700

    [REM] simple_manufacture: * replace constraints to production_detail_ids * remove field to display sum_percent_weights replaced with sum attrs in item.production.detail tree view.

--- simple_manufacture/models/item_production.py
+++ simple_manufacture/models/item_production.py
@@ -13,21 +13,21 @@ class ItemProduction(models.Model):
     date_finish = fields.Datetime()
     est_date_finish = fields.Datetime(store=False)
     production_detail_ids = fields.One2many('item.production.detail', inverse_name='production_id')
-    sum_percent_weights = fields.Float(store=False, readonly=True, compute='_compute_sum_percent_weight')
+    # sum_percent_weights = fields.Float(store=False, readonly=True, compute='_compute_sum_percent_weight')
 
-    @api.multi
-    def _compute_sum_percent_weight(self):
-        self.sum_percent_weights = self.count_sum_percent_weights()
+    # @api.multi
+    # def _compute_sum_percent_weight(self):
+    #     self.sum_percent_weights = self.count_sum_percent_weights()
 
-    @api.constrains('sum_percent_weights')
-    def _check_seats_limit(self):
-        if self.sum_percent_weights > 100 or self.sum_percent_weights < 100:
+    @api.constrains('production_detail_ids')
+    def _check_production_detail_ids(self):
+        if not self.count_sum_percent_weights() == 100:
             err_msg = _('Total Weights (%) is not equal to 100%')
             raise ValidationError(err_msg)
 
-    @api.onchange('production_detail_ids')
-    def _onchange_production_detail_ids(self):
-        self.sum_percent_weights = self.count_sum_percent_weights()
+    # @api.onchange('production_detail_ids')
+    # def _onchange_production_detail_ids(self):
+    #     self.sum_percent_weights = self.count_sum_percent_weights()
 
     def count_sum_percent_weights(self):
         sum_weights = 0
