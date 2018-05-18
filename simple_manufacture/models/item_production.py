f170714df4f6a052c7a605918227c96d2d8f9cb4
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-19 01:26:57 +0700

    [ADD] simple_manufacture: add compute sum_percent_weights and show it only in edit mode.

--- simple_manufacture/models/item_production.py
+++ simple_manufacture/models/item_production.py
@@ -11,9 +11,13 @@ class ItemProduction(models.Model):
     name = fields.Char(string='Item Name')
     date_start = fields.Datetime(string='Date Start')
     date_finish = fields.Datetime()
-    est_date_finish = fields.Datetime(stored=False)
+    est_date_finish = fields.Datetime(store=False)
     production_detail_ids = fields.One2many('item.production.detail', inverse_name='production_id')
-    sum_percent_weights = fields.Float(stored=False)
+    sum_percent_weights = fields.Float(store=False, readonly=True, compute='_compute_sum_percent_weight')
+
+    @api.multi
+    def _compute_sum_percent_weight(self):
+        self.sum_percent_weights = self.count_sum_percent_weights()
 
     @api.constrains('sum_percent_weights')
     def _check_seats_limit(self):
@@ -23,10 +27,13 @@ class ItemProduction(models.Model):
 
     @api.onchange('production_detail_ids')
     def _onchange_production_detail_ids(self):
+        self.sum_percent_weights = self.count_sum_percent_weights()
+
+    def count_sum_percent_weights(self):
         sum_weights = 0
         for component in self.production_detail_ids:
             sum_weights += component.percent_weights
-        self.sum_percent_weights = sum_weights
+        return sum_weights
 
 
 class ItemProductionDetail(models.Model):
