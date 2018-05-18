518e98c31f2fdb05a14999be09318204fae6da26
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-19 01:16:02 +0700

    [ADD] simple_manufacture: add constraints to sum_percent_weights.

--- simple_manufacture/models/item_production.py
+++ simple_manufacture/models/item_production.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from openerp import models, fields, api, exceptions, _
+from openerp.exceptions import ValidationError
 
 
 class ItemProduction(models.Model):
@@ -12,9 +13,14 @@ class ItemProduction(models.Model):
     date_finish = fields.Datetime()
     est_date_finish = fields.Datetime(stored=False)
     production_detail_ids = fields.One2many('item.production.detail', inverse_name='production_id')
-    sum_percent_weights = fields.Float()
+    sum_percent_weights = fields.Float(stored=False)
 
-    @api.multi
+    @api.constrains('sum_percent_weights')
+    def _check_seats_limit(self):
+        if self.sum_percent_weights > 100 or self.sum_percent_weights < 100:
+            err_msg = _('Total Weights (%) is not equal to 100%')
+            raise ValidationError(err_msg)
+
     @api.onchange('production_detail_ids')
     def _onchange_production_detail_ids(self):
         sum_weights = 0
@@ -27,6 +33,6 @@ class ItemProductionDetail(models.Model):
     _name = 'item.production.detail'
     _description = 'Item Production Detail'
 
-    production_id = fields.Many2one('item.production', ondelete='cascade')
-    component_ids = fields.Many2one('master.component', 'Component Name')
-    percent_weights = fields.Float('Percent Weight')
+    production_id = fields.Many2one('item.production', ondelete='cascade', required=True)
+    component_ids = fields.Many2one('master.component', 'Component Name', required=True)
+    percent_weights = fields.Float('Percent Weight', required=True)
