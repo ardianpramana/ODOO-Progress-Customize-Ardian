76933032e66ace142ba3f07d770f4313583f1fb8
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-19 09:06:50 +0700

    [ADD] simple_manufacture: add check duplicate component constraints.

--- simple_manufacture/models/item_production.py
+++ simple_manufacture/models/item_production.py
@@ -13,27 +13,44 @@ class ItemProduction(models.Model):
     date_finish = fields.Datetime()
     est_date_finish = fields.Datetime(store=False)
     production_detail_ids = fields.One2many('item.production.detail', inverse_name='production_id')
-    # sum_percent_weights = fields.Float(store=False, readonly=True, compute='_compute_sum_percent_weight')
-
-    # @api.multi
-    # def _compute_sum_percent_weight(self):
-    #     self.sum_percent_weights = self.count_sum_percent_weights()
 
     @api.constrains('production_detail_ids')
     def _check_production_detail_ids(self):
+        """
+        Condition check before writing documents.
+        """
+        if not self.check_duplicate_component():
+            err_msg = _('There are duplicated component selected.')
+            raise ValidationError(err_msg)
+
         if not self.count_sum_percent_weights() == 100:
             err_msg = _('Total Weights (%) is not equal to 100%')
             raise ValidationError(err_msg)
 
-    # @api.onchange('production_detail_ids')
-    # def _onchange_production_detail_ids(self):
-    #     self.sum_percent_weights = self.count_sum_percent_weights()
-
     def count_sum_percent_weights(self):
+        """
+        Count sum of percent_weights for all selected components.
+        """
         sum_weights = 0
         for component in self.production_detail_ids:
             sum_weights += component.percent_weights
         return sum_weights
+
+    def check_duplicate_component(self):
+        """
+        Check if there is duplicate component selected.
+        """
+        component_ids = []
+        for component in self.production_detail_ids:
+            component_ids.append(component.component_id.id)
+
+        if component_ids:
+            unique = set(component_ids)
+            for item in unique:
+                count = component_ids.count(item)
+                if count > 1:
+                    return False
+        return True
 
 
 class ItemProductionDetail(models.Model):
@@ -41,5 +58,5 @@ class ItemProductionDetail(models.Model):
     _description = 'Item Production Detail'
 
     production_id = fields.Many2one('item.production', ondelete='cascade', required=True)
-    component_ids = fields.Many2one('master.component', 'Component Name', required=True)
+    component_id = fields.Many2one('master.component', 'Component Name', required=True)
     percent_weights = fields.Float('Percent Weight', required=True)
--- simple_manufacture/models/master_component.py
+++ simple_manufacture/models/master_component.py
@@ -9,10 +9,3 @@ class MasterComponent(models.Model):
 
     name = fields.Char(string='Component Name')
     estimation_time = fields.Float(string='Estimation Time')
-
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
