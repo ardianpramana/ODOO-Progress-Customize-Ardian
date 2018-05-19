6df1f6afbff4f89ca7c1c0a5b2aa8f1cec7d25da
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-20 00:47:14 +0700

    [ADD] simple_manufacture: * add sequence to item.productions, * add view days per component & item_code.

--- simple_manufacture/models/item_production.py
+++ simple_manufacture/models/item_production.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 
 from openerp import models, fields, api, exceptions, _
@@ -10,11 +10,11 @@ class ItemProduction(models.Model):
     _name = 'item.production'
     _description = 'Item Production'
 
+    sequence_name = fields.Char(default='New')
     name = fields.Char(required=True)
     date_start = fields.Datetime(required=True)
     date_finish = fields.Datetime()
     est_date_finish = fields.Datetime(readonly=True, compute='_compute_est_date_finish', store=False)
-    # time_delta = fields.Float(store=False, readonly=True)
     production_detail_ids = fields.One2many('item.production.detail', inverse_name='production_id')
 
     @api.depends('production_detail_ids', 'date_start')
@@ -24,14 +24,14 @@ class ItemProduction(models.Model):
         """
         for rec in self:
             component_time = 0
-            est_date = datetime.today()
+            # est_date = datetime.today()
             for component in rec.production_detail_ids:
                 component_id = self.env['master.component'].search([['id', '=', component.component_id.id]])
                 component_time += component_id.estimation_time
 
             if component_time:
                 est_date = (datetime.strptime(rec.date_start, '%Y-%m-%d %H:%M:%S') + relativedelta(days=component_time))
-            rec.est_date_finish = est_date.isoformat(' ')
+                rec.est_date_finish = est_date.isoformat(' ')
 
     @api.constrains('production_detail_ids')
     def _check_production_detail_ids(self):
@@ -49,6 +49,16 @@ class ItemProduction(models.Model):
         if not self.count_sum_percent_weights() == 100:
             err_msg = _('Total Weights (%) is not equal to 100%')
             raise ValidationError(err_msg)
+
+    @api.model
+    def create(self, vals):
+        """ Get sequence for item_production"""
+        current_date = date.today().isoformat()
+        if vals.get('sequence_name', 'New') == 'New':
+            seq_obj = self.env['ir.sequence']
+            vals['sequence_name'] = seq_obj.with_context(ir_sequence_date=current_date).next_by_code('item.production') \
+                                    or '/'
+        return super(ItemProduction, self).create(vals)
 
     def count_sum_percent_weights(self):
         """
@@ -82,4 +92,5 @@ class ItemProductionDetail(models.Model):
 
     production_id = fields.Many2one('item.production', ondelete='cascade', required=True)
     component_id = fields.Many2one('master.component', required=True)
+    component_days = fields.Integer(related='component_id.estimation_time', store=False)
     percent_weights = fields.Float('Percent Weight', required=True)
