c20f8c47d0cc0c7d1de0d3c8b9fc9622d6205eba
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-20 00:12:10 +0700

    [REF] simple_manufacture: refactor master.component model and view.

--- simple_manufacture/models/master_component.py
+++ simple_manufacture/models/master_component.py
@@ -9,25 +9,30 @@ class MasterComponent(models.Model):
     _description = 'Master Component'
 
     sequence_name = fields.Char(default='New')
-    date = fields.Date("Date", default=date.today(), required=True, readonly=True)
     name = fields.Char(required=True)
-    estimation_time = fields.Float(required=True)
+    estimation_time = fields.Integer(required=True)
 
-    # @api.constrains('name')
-    # def _check_name(self):\
-    #     master_component_ids = self.search([['id', '!=', self.id]])
-    #     print master_component_ids
-        # for record in master_component_ids:
-        #     if record.name == self.name:
-        #         err_msg = _('Current component name is already in the master data. '
-        #                     'Please choose another component name!')
-        #         raise ValidationError(err_msg)
+    @api.constrains('name')
+    def _check_name(self):
+        master_component_ids = self.search([['id', '!=', self.id]])
+        for record in master_component_ids:
+            if record.name == self.name:
+                err_msg = _('Current component name is already in the master data. '
+                            'Please choose another component name!')
+                raise ValidationError(err_msg)
+
+    @api.constrains('estimation_time')
+    def _check_estimation_time(self):
+        if not self.estimation_time > 0:
+            err_msg = _('Please input more than 0 days')
+            raise ValidationError(err_msg)
 
     @api.model
     def create(self, vals):
         """ Get sequence for master_component"""
+        current_date = date.today().isoformat()
         if vals.get('sequence_name', 'New') == 'New':
             seq_obj = self.env['ir.sequence']
-            vals['sequence_name'] = seq_obj.with_context(ir_sequence_date=vals['date']).next_by_code('master.component') \
+            vals['sequence_name'] = seq_obj.with_context(ir_sequence_date=current_date).next_by_code('master.component') \
                                     or '/'
         return super(MasterComponent, self).create(vals)
